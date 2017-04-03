58f0c2825e3c34680ad284576599094730a41177
muffe <fredrikdel@gmail.com>
2017-04-03 22:01:19 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var conf = require('config.json');
+function parseObject(obj) {
+  var keys = Object.keys(obj);
+  for (keys in obj) {
+    if (typeof obj[keys] === "string") {
+      console.log(keys + " " + obj[keys] + ";");
+    }
+    else if (typeof obj[keys] == "object" && !Array.isArray(obj[keys])) {
+      parseObject(obj[keys]);
+    }
+    else if (Array.isArray(obj[keys])) {
+      for (i=0; i<obj[keys].length; i++) {
+        if (typeof obj[keys][i] === "string") {
+          console.log(keys + " " + obj[keys][i] + ";");
+        }
+        else if (obj[keys][i].data != undefined) {
+          if (obj[keys][i].args != undefined) { console.log(keys + " " + obj[keys][i].args + " {") }
+          else { console.log(keys + " {") }
+          parseObject(obj[keys][i].data);
+          console.log("}")
+        }
+        else parseObject(obj[keys]);
+      }
+    }
+  }
+}
+
+parseObject(conf);
